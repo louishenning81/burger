e98f69193f1e245be1a62e35174b81869026976b
louishenning81 <lhhenning@gmail.com>
2019-04-05 09:39:22 -0700

    created server.js, connection.js and orm.js

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger w Brie on Brioche');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger with cheddar and bacon on Sesame seed');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger on sesame with onions');
+INSERT INTO burgers (burger_name) VALUES ('Beyond Burger with Havarti');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger on sesame');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger with Pepper Jack and Avocado on Sesame');
